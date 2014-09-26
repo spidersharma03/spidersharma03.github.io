02ee46f79b2bcfd1bf7d11541980ad9446d7af6f
spidersharma03 <namitasharma172@gmail.com>
2014-09-26 16:52:20 +0530

    Simple Pendulumn class
    Simple Pendulumn class added to simplify Pendulumn creation.
    for ex. Two SimplePendulumns will be created to demonstrate Phse shift
    between them.

---
+++ ScienceLab/public_html/simulations/fundamental_quantities/SimplePendulumn.js
@@ -0,0 +1,38 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+SimplePendulumn = function()
+{
+    this.origin = new THREE.Vector2();
+    this.position = new THREE.Vector2();
+    this.length = 1.8;
+    this.time = 0.0;
+    this.maxTheta = 15;
+};
+
+SimplePendulumn.prototype = {
+    constructor: SimplePendulumn,
+    
+    setLength : function(length)
+    {
+        this.length = length;
+    },
+    
+    setOrigin : function(origin)
+    {
+        this.origin.x = origin.x;
+        this.origin.y = origin.y;
+    },
+    
+    update : function(dt)
+    {
+        var theta = this.maxTheta * Math.sin(this.time*3) * 3.14/180;
+        this.position.x = this.origin.x + this.length * Math.sin(theta);
+        this.position.y = this.origin.y + -this.length * Math.cos(theta);
+        this.time += dt;
+    }
+};

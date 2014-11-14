c3f2622b0e606221394915a8cbf45659b0185a32
unknown <PTS@BANWOR301.ESI-INTERNAL.esi-group.com>
2014-11-14 19:02:50 +0530

    Simple Pendulumn Energy Bars.

--- ScienceLab/public_html/simulations/fundamental_quantities/SimplePendulumn.js
+++ ScienceLab/public_html/simulations/fundamental_quantities/SimplePendulumn.js
@@ -12,7 +12,7 @@ SimplePendulumn = function()
     this.length = 1.8;
     this.time = 0.0;
     this.theta = 0.0;
-    this.angularSpeed = 0.0;
+    this.speed = 0.0;
     this.acceleration = 0.0;
     this.pe = 0.0; // Potential energy
     this.ke = 0.0; // Kinetic energy
@@ -30,7 +30,7 @@ SimplePendulumn = function(length)
     this.length = length;
     this.time = 0.0;
     this.theta = 0.0;
-    this.angularSpeed = 0.0;
+    this.speed = 0.0;
     this.acceleration = 0.0;
     this.pe = 0.0; // Potential energy
     this.ke = 0.0; // Kinetic energy
@@ -72,16 +72,17 @@ SimplePendulumn.prototype = {
     {
         var phase_ = this.time*this.omega;
         this.theta = this.maxTheta * Math.sin( phase_ + this.phase*Math.PI/180) * Math.PI/180;
-        this.angularSpeed = this.maxTheta * this.omega * Math.cos( phase_ + this.phase*Math.PI/180) * Math.PI/180;
-        this.acceleration = -this.maxTheta * this.omega * this.omega * Math.sin( phase_ + this.phase*Math.PI/180) * Math.PI/180;
+        this.speed = this.maxTheta * this.length * this.omega * Math.cos( phase_ + this.phase*Math.PI/180) * Math.PI/180;
+        this.acceleration = -this.maxTheta * this.length * this.omega * this.omega * Math.sin( phase_ + this.phase*Math.PI/180) * Math.PI/180;
         this.position.x = this.origin.x + this.length * Math.sin(this.theta);
         this.position.y = this.origin.y + -this.length * Math.cos(this.theta);
-        updateEnergies(dt);
+        this.updateEnergies(dt);
         this.time += dt;
     },
     
     updateEnergies : function(dt)
     {
         this.pe = this.g * this.length * ( 1.0 - Math.cos(this.theta)); // mgl(1-cos(theta))
+        this.ke = 0.5 * this.speed * this.speed; // 1/2*m*v2
     }
 };

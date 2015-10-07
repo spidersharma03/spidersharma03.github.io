ce62465b8113dabf94a198eae68a5de85904232d
Prashant <pts@esi-group.com>
2015-10-07 13:30:36 +0530

    * Temp commit

--- ScienceLab/public_html/js/mainApp.js
+++ ScienceLab/public_html/js/mainApp.js
@@ -54,6 +54,7 @@ var mainApp = angular.module('mainApp', ['ngRoute', 'DataLoadControllers', 'Cont
  
 //
 mainApp.controller('homePageLoadController', function ($scope, $http, $route) {
+        return;
         $scope.logged_in = false;
         $scope.login_emptyEmail = false;
         $scope.login_emptyPassword = false;
